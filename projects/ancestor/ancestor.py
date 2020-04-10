d14586e208bb5bade9ba941ed0b4cdf6449cb0cc
Ira <ira.d.sanchez@gmail.com>
2020-04-09 19:15:34 -0700

    Getting started on ancestor
    - struggled a bit with imports
    - decided to just make a graph of the graph file and move it over

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,16 @@
+from graph import Graph
+
+import sys
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+
+    family_tree = Graph()
+
+    for parent_child_pair in ancestors:
+        family_tree.add_vertex(parent_child_pair[0])
+        print("ADD PARENT VERTEX", family_tree.vertices)
+        family_tree.add_vertex(parent_child_pair[1])
+        print("ADD CHILD VERTEX", family_tree.vertices)
+        family_tree.add_edge(parent_child_pair[0], parent_child_pair[1])
+        print("ADD EDGE", family_tree.vertices)
